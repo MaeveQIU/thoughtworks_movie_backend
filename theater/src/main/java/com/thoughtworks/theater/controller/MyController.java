44753f80d4bc3e27e7474bcd8b5180bea0c1ae2a
Maeve QIU <maeve.cs23@gmail.com>
2020-04-14 16:04:06 +0800

    [Shi QIU]{feat}: add controller feature

--- theater/src/main/java/com/thoughtworks/theater/controller/MyController.java
+++ theater/src/main/java/com/thoughtworks/theater/controller/MyController.java
@@ -1,8 +1,15 @@
 package com.thoughtworks.theater.controller;
+import com.thoughtworks.theater.entity.Movie;
 import com.thoughtworks.theater.service.TheaterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RequestMapping("movies")
 @RestController
 public class MyController {
   //get all movie, return List<Movie>
@@ -12,5 +19,20 @@ public class MyController {
   // 传回去的值里genres等都是string，前端得改！
   @Autowired
   private TheaterService userService;
+
+  @GetMapping("/get/all")
+  public List<Movie> getAll() {
+    return userService.getAllMovies();
+  }
+
+  @GetMapping("/get/{title}")
+  public List<Movie> getByTitle(@PathVariable String title) {
+    List<Movie> movies = userService.findByMovieNameLike(title);
+    if (movies.isEmpty()) {
+      return null;
+    } else {
+      return movies;
+    }
+  }
 
 }
--- theater/src/main/java/com/thoughtworks/theater/entity/Movie.java
+++ theater/src/main/java/com/thoughtworks/theater/entity/Movie.java
@@ -29,6 +29,22 @@ public class Movie {
     this.title = title;
   }
 
+  public int getMovieId() {
+    return movieId;
+  }
+
+  public void setMovieId(int movieId) {
+    this.movieId = movieId;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
   public int getYear() {
     return year;
   }
