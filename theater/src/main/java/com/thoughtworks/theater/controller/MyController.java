6a69594bcae3200a515591f27722c61207e83527
Xiao Yu <xiao.susan.yu@gmail.com>
2020-04-14 17:30:55 +0800

    [Xiao]{fix}: enable crossOrigin

--- theater/src/main/java/com/thoughtworks/theater/controller/MyController.java
+++ theater/src/main/java/com/thoughtworks/theater/controller/MyController.java
@@ -2,13 +2,11 @@ package com.thoughtworks.theater.controller;
 import com.thoughtworks.theater.entity.Movie;
 import com.thoughtworks.theater.service.TheaterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("movies")
 @RestController
 public class MyController {
